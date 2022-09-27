e0b237a37adff75727361f56fdffe24296475455
Beaman <rbeam314@uwsp.edu>
2022-09-26 19:32:51 -0500

    HTML with PHP

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+print "<!doctype html>";
+print "<html>";
+
+print "<head>";
+	print "<title>Week 3 Lecture 2</title>";
+	print "<link rel=\"stylesheet\" href=\"css.css?" . "q=" . time() . "\">\n";
+print "</head>";
+
+print "<body>";
+	print "<h1 class=\"first\"> CNMT 310 </h1>\n";
+	print "<p> My name: Rayna Beaman </p>\n";
+	print "<h1 class=\"second\"> HTML Lab </h1>\n";
+	
+	print "<div id=\"browser\"> </div>";
+	print "<script src\"script.js\"></script>\n";
+print "</body>";
+
+print "</html>";
+
+?>
---
+++ script.js
@@ -0,0 +1,8 @@
+
+let printData = function(data) {
+	document.getElementById("browser").innerHTML = navigator.userAgent;
+} 
+let divInfo = function(){
+printData("browser");
+}
+
