49aee4a81675e4500cfc533a3880f451f33fe711
Beaman <rbeam314@uwsp.edu>
2022-10-05 13:30:55 -0500

    labSession

---
+++ sessionlab.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start();
+
+$_SESSION['firstVisit'] = (date("Y-m-d H:i:s", $_SERVER['REQUEST_TIME']));
+
+if (isset($_SERVER['REQUEST_TIME'])){
+		$_SESSION['firstVisit'];
+}	
+	print ("The current time on the server is: "). $_SESSION['firstVisit'];
+
+echo '<br></br><a href="sessionlab2.php">Go to Page 2</a>.';
+
+?>
